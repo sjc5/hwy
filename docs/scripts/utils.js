6ca0649d467321b429f3ff877c1414e429452c09
Sam Cook <email@sjc.sh>
2023-10-28 16:38:35 -0500

    docs to latest and better docs scripts

--- docs/scripts/utils.js
+++ docs/scripts/utils.js
@@ -1,20 +1,22 @@
 import fs from "node:fs";
 
-const current_docs_hwy_version = "0.5.0-beta.0";
+const current_docs_hwy_version = "0.5.0-beta.5";
+
+const hwy_pkg_regex = /"hwy": "([^"]+)"/;
+const build_pkg_regex = /"@hwy-js\/build": "([^"]+)"/;
+const dev_pkg_regex = /"@hwy-js\/dev": "([^"]+)"/;
+
+function replace_versions(pkg_json_string, version) {
+  return pkg_json_string
+    .replace(hwy_pkg_regex, `"hwy": "${version}"`)
+    .replace(build_pkg_regex, `"@hwy-js/build": "${version}"`)
+    .replace(dev_pkg_regex, `"@hwy-js/dev": "${version}"`);
+}
 
 function to_workspace() {
   let pkg_json_string = fs.readFileSync("package.json", "utf-8");
 
-  pkg_json_string = pkg_json_string
-    .replace(`"hwy": "${current_docs_hwy_version}"`, `"hwy": "workspace:*"`)
-    .replace(
-      `"@hwy-js/build": "${current_docs_hwy_version}"`,
-      `"@hwy-js/build": "workspace:*"`,
-    )
-    .replace(
-      `"@hwy-js/dev": "${current_docs_hwy_version}"`,
-      `"@hwy-js/dev": "workspace:*"`,
-    );
+  pkg_json_string = replace_versions(pkg_json_string, "workspace:*");
 
   fs.writeFileSync("package.json", pkg_json_string);
 
@@ -24,16 +26,7 @@ function to_workspace() {
 function to_latest() {
   let pkg_json_string = fs.readFileSync("package.json", "utf-8");
 
-  pkg_json_string = pkg_json_string
-    .replace(`"hwy": "workspace:*"`, `"hwy": "${current_docs_hwy_version}"`)
-    .replace(
-      `"@hwy-js/build": "workspace:*"`,
-      `"@hwy-js/build": "${current_docs_hwy_version}"`,
-    )
-    .replace(
-      `"@hwy-js/dev": "workspace:*"`,
-      `"@hwy-js/dev": "${current_docs_hwy_version}"`,
-    );
+  pkg_json_string = replace_versions(pkg_json_string, current_docs_hwy_version);
 
   fs.writeFileSync("package.json", pkg_json_string);
 
