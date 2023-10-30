28f8411f143c58ce374d8262038e342fe1f4a7b1
Sam Cook <email@sjc.sh>
2023-10-29 21:35:33 -0500

    docs: restore Prism event listener

--- docs/src/client.entry.ts
+++ docs/src/client.entry.ts
@@ -1,3 +1,7 @@
 import { initHtmx, initIdiomorph, initNProgress } from "@hwy-js/client";
 
 initHtmx().then(initIdiomorph).then(initNProgress);
+
+document.addEventListener("htmx:afterSettle", () => {
+  (window as any).Prism.highlightAll();
+});
