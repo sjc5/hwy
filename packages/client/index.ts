d586d44a1064fd2b15abd061653649fd2391000d
Sam Cook <email@sjc.sh>
2023-11-11 14:06:04 -0600

    cookie events

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,17 +1,3 @@
-async function initHtmx() {
-  const htmx = await import("htmx.org").catch();
-  (window as any).htmx = htmx;
-
-  await import("htmx.org/dist/ext/head-support.js" as any).catch();
-}
-
-async function initIdiomorph() {
-  await import("idiomorph/dist/idiomorph-ext.js" as any).catch();
-}
-
-async function initNProgress() {
-  const NProgress = await import("nprogress").catch();
-  (window as any).NProgress = NProgress;
-}
-
-export { initHtmx, initIdiomorph, initNProgress };
+export { initHtmx, initIdiomorph, initNProgress } from "./src/initers.js";
+export { makeCookieEventHelpers } from "./src/client-cookie-events.js";
+export type { CookieEvents } from "./src/client-cookie-events.js";
