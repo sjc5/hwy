cc97f12427510fdca5ee6d233685b43a728f50ca
Sam Cook <email@sjc.sh>
2023-12-12 00:22:43 -0600

    sp

---
+++ packages/core/src/preact/head-elements-comp.tsx
@@ -0,0 +1,102 @@
+import {
+  BaseProps,
+  sort_head_blocks,
+  get_hwy_global,
+} from "../../../common/index.mjs";
+import { utils } from "../utils/hwy-utils.js";
+
+const hwy_global = get_hwy_global();
+
+function HeadElements(props: BaseProps) {
+  const IS_PREACT_MPA = hwy_global.get("mode") === "preact-mpa";
+
+  const head_blocks = utils.getHeadBlocks(props);
+
+  const { title, metaHeadBlocks, restHeadBlocks } =
+    sort_head_blocks(head_blocks);
+
+  const timeout = undefined; // TODO -- move timeout to HwyConfig.dev and read here
+  const dev_refresh_script = utils.getRefreshScript(timeout);
+
+  const critical_css = utils.getCriticalCss();
+  const standard_bundled_css_exists = hwy_global.get(
+    "standard_bundled_css_exists",
+  );
+
+  return (
+    <>
+      {title && <title>{title}</title>}
+
+      <meta data-hwy="meta-start" />
+
+      {metaHeadBlocks.map((block) => {
+        return (
+          // @ts-ignore
+          <block.tag
+            //
+            {...block.attributes}
+          />
+        );
+      })}
+
+      <meta data-hwy="meta-end" />
+
+      {critical_css && (
+        <style
+          id={utils.getCriticalCssElementId()}
+          dangerouslySetInnerHTML={{
+            __html: critical_css,
+          }}
+        />
+      )}
+      {standard_bundled_css_exists && (
+        <link rel="stylesheet" href={utils.getBundledCssUrl()} />
+      )}
+
+      {IS_PREACT_MPA && (
+        <script
+          type="importmap"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(utils.getImportMap()),
+          }}
+        />
+      )}
+
+      {IS_PREACT_MPA && (
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: utils.getSsrInnerHtml(props.activePathData),
+          }}
+        />
+      )}
+
+      <script type="module" src={utils.getClientEntryUrl()} />
+
+      <meta data-hwy="rest-start" />
+
+      {restHeadBlocks.map((block) => {
+        return (
+          // @ts-ignore
+          <block.tag
+            //
+            {...block.attributes}
+          />
+        );
+      })}
+
+      <meta data-hwy="rest-end" />
+
+      {dev_refresh_script && (
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: dev_refresh_script,
+          }}
+        />
+      )}
+    </>
+  );
+}
+
+export { HeadElements };
---
+++ packages/core/src/preact/render-root.tsx
@@ -0,0 +1,75 @@
+import {
+  BaseProps,
+  HWY_PREFIX,
+  sort_head_blocks,
+  get_hwy_global,
+  HeadBlock,
+} from "../../../common/index.mjs";
+import { Context, Next } from "hono";
+import { renderToString } from "preact-render-to-string";
+import { JSX } from "preact";
+import { getMatchingPathData } from "../router/get-matching-path-data.js";
+import { utils } from "../utils/hwy-utils.js";
+
+async function renderRoot({
+  c,
+  next,
+  defaultHeadBlocks,
+  root: Root,
+}: {
+  c: Context;
+  next: Next;
+  defaultHeadBlocks?: HeadBlock[];
+  root: (baseProps: BaseProps) => JSX.Element;
+}) {
+  const activePathData = await getMatchingPathData({ c });
+
+  if (activePathData.fetchResponse) {
+    return activePathData.fetchResponse;
+  }
+
+  if (!activePathData.matchingPaths?.length) {
+    return await next();
+  }
+
+  const IS_PREACT_MPA = get_hwy_global().get("mode") === "preact-mpa";
+
+  if (IS_PREACT_MPA) {
+    const IS_JSON = Boolean(c.req.query()[`${HWY_PREFIX}json`]);
+
+    if (IS_JSON) {
+      const baseProps = { c, activePathData, defaultHeadBlocks };
+
+      if (c.req.raw.signal.aborted) {
+        return;
+      }
+
+      const headBlocks = utils.getHeadBlocks(baseProps);
+      const { title, metaHeadBlocks, restHeadBlocks } =
+        sort_head_blocks(headBlocks);
+
+      return c.json({
+        title,
+        metaHeadBlocks,
+        restHeadBlocks,
+        activeData: activePathData.activeData,
+        activePaths: activePathData.matchingPaths?.map((x) => {
+          return utils.getPublicUrl(
+            "dist/" + x.importPath.slice(0, -3) + ".hydrate.js",
+          );
+        }),
+        outermostErrorBoundaryIndex: activePathData.outermostErrorBoundaryIndex,
+        errorToRender: activePathData.errorToRender,
+        splatSegments: activePathData.splatSegments,
+        params: activePathData.params,
+        actionData: activePathData.actionData,
+      });
+    }
+  }
+
+  const base_props = { c, activePathData, defaultHeadBlocks };
+  const markup = <Root {...base_props} />;
+  return c.html("<!doctype html>" + renderToString(markup));
+}
+
+export { renderRoot };
