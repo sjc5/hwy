17b0f11cb4bff9aab74b458e058863d149dad093
Sam Cook <email@sjc.sh>
2024-04-29 20:47:20 -0500

    no need to use startTransition from preact/compat

--- examples/preact/src/entry.client.tsx
+++ examples/preact/src/entry.client.tsx
@@ -3,19 +3,17 @@ if (process.env.NODE_ENV === "development") {
 }
 import { initClient } from "@hwy-js/client";
 import { RootOutlet } from "@hwy-js/react";
-import { StrictMode, hydrate, startTransition } from "preact/compat";
+import { StrictMode, hydrate } from "preact/compat";
 
 await initClient(() => {
-  startTransition(() => {
-    hydrate(
-      <StrictMode>
-        <RootOutlet
-          fallbackErrorBoundary={function ErrorBoundary() {
-            return <div>Something went wrong.</div>;
-          }}
-        />
-      </StrictMode>,
-      document.querySelector("main") as HTMLElement,
-    );
-  });
+  hydrate(
+    <StrictMode>
+      <RootOutlet
+        fallbackErrorBoundary={function ErrorBoundary() {
+          return <div>Something went wrong.</div>;
+        }}
+      />
+    </StrictMode>,
+    document.querySelector("main") as HTMLElement,
+  );
 });
