5ad6cd39f52cd4a3ce8b23120fa260e1e737e157
Sam Cook <email@sjc.sh>
2024-04-29 19:55:26 -0500

    v0.13.0

--- testers/routes/src/pages/lion.data.ts
+++ testers/routes/src/pages/lion.data.ts
@@ -1,16 +1,17 @@
-import { HeadProps } from "hwy";
+import { DataProps, HeadProps } from "hwy";
 
 export function head(props: HeadProps<typeof loader>) {
   console.log(props.loaderData.data.lion);
   return { title: "Lion" };
 }
 
-export const loader = async () => {
-  if (Math.random() < 0.5) {
-    throw new Response("", {
-      status: 302,
-      headers: { location: "/bob" },
-    });
-  }
+export const loader = async (props: DataProps) => {
+  // throw new Response("", {
+  //   status: 302,
+  //   headers: { location: "/bob" },
+  // });
+  const headers = new Headers();
+  headers.set("x-lion", "asdf");
+  props.responseInit.headers = headers;
   return { data: { lion: "🦁" } };
 };
