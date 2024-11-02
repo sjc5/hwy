9201a47edd1fec92ba0f42950a9c2e8f9d2790e3
Sam Cook <email@sjc.sh>
2024-11-02 13:58:53 -0500

    fix

--- scripts/npm/utils.ts
+++ scripts/npm/utils.ts
@@ -2,7 +2,7 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import * as readline from "node:readline";
 
-const dirsInSlashPackages = ["create", "react" /* "client" */];
+const dirsInSlashPackages = ["client", "react" /* "create" */];
 const preSuffix = "-pre";
 
 function getCurrentPkgJSONs() {
